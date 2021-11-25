749009651aabb909e8abcf123149a9a9aa3ad086
urdeathqt <ediken994@gmail.com>
2021-11-25 03:50:52 +0000

    🤡 Inicio del proyecto

---
+++ application.py
@@ -0,0 +1,122 @@
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import login_required
+
+app = Flask(__name__)
+
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///gaming.db")
+
+@app.route("/")
+@login_required
+def index():
+    return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            flash("must provide username")
+            return render_template("login.html")
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            flash("must provide password")
+            return render_template("login.html")
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM usuarios WHERE username = :username",
+                          username=request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["contraseña"], request.form.get("password")):
+            flash("invalid username and/or password")
+            return render_template("login.html")
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect(url_for("index"))
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect(url_for("index"))
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+
+    if request.method == "POST":
+
+        if not request.form.get("username"):
+            flash("Usted debe ingresar un usuario")
+            return render_template("register.html")
+
+        elif not request.form.get("password"):
+            flash("Usted debe ingresar una contraseña")
+            return render_template("register.html")
+
+        elif not request.form.get("confirmation"):
+            flash("Debe proporcionar la contraseña de confirmacion")
+            return render_template("register.html")
+
+        if not request.form.get("password") == request.form.get("confirmation"):
+            flash("Las contraseñas no son las mismas")
+            return render_template("register.html")
+
+        hash = generate_password_hash(request.form.get("password"))
+        new_user_id = db.execute("INSERT INTO usuarios (username, contraseña) VALUES(:username, :hash)",
+                                 username=request.form.get("username"),
+                                 hash=hash)
+
+        if not new_user_id:
+            flash("Usuario ya usado")
+            return render_template("register.html")
+
+        session["user_id"] = new_user_id
+
+        flash("Registrado!")
+
+        return redirect(url_for("index"))
+
+    else:
+        return render_template("register.html")
+
